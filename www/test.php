9f1e53b6c42873ca0c233505959da70164d621b0
dammy <adiodamilola@yahoo.com>
2017-03-29 02:13:25 +0100

    pdo exception handling

--- www/test.php
+++ www/test.php
@@ -4,6 +4,16 @@
 	define('DBUSER', 'root');
 	define('DBPASS', 'damilolo');
 
+	try{
+
+	# prepare a PDO instance	
 	$conn = new PDO("mysql:host=localhost;dbname=".DBNAME, DBUSER, DBPASS);
+
+	# set verbose error modes
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+	} catch(PDOException $e){
+		echo $e->getMessage();
+	}
 
 ?>
