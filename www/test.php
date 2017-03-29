601ed5df5c86f8b7f6c97322d36aa0752942607f
dammy <adiodamilola@yahoo.com>
2017-03-29 01:59:28 +0100

    use constants for db connection

--- www/test.php
+++ www/test.php
@@ -1,5 +1,9 @@
 <?php
 
-	$conn = new PDO("mysql:host=localhost;dbname=online_store", "root", "damilolo");
+	define('DBNAME', 'online_store');
+	define('DBUSER', 'root');
+	define('DBPASS', 'damilolo');
+
+	$conn = new PDO("mysql:host=localhost;dbname=".DBNAME, DBUSER, DBPASS);
 
 ?>
