796f1a8a2e3d8ecd8a7f4a8021a830a3dc4c7b90
dammy <adiodamilola@yahoo.com>
2017-03-29 03:20:12 +0100

    continue validation

--- www/test.php
+++ www/test.php
@@ -16,8 +16,30 @@
 		echo $e->getMessage();
 	}    */
 
+	# max file size
+	define('MAX_FILE_SIZE', '2097152');
+
 	if(array_key_exists('save', $_POST)){
-		print_r($_FILES);
+		$errors = [];
+
+		#be sure a file was selected...
+		if(empty($_FILES['pic']['name'])){
+			$errors['pic'] = "Please choose a file";
+		}
+
+		# check file size...
+		if($_FILES['pic']['size'] > MAX_FILE_SIZE){
+			$errors['pic'] = "file size exceeds maximum. maximum: ". MAX_FILE_SIZE;
+		}
+
+		if(empty($errors)){
+			echo "done";
+		}else {
+			foreach($errors as $err){
+				echo $err. '</br>';
+			}
+		}
+
 	}
 ?>
 
