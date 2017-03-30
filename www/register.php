93d180f8e6b73787087e9de66918066217f94311
dammy <adiodamilola@yahoo.com>
2017-03-30 03:36:07 +0100

    little changes

--- www/register.php
+++ www/register.php
@@ -57,21 +57,36 @@ if(array_key_exists('register', $_POST)){
 				<label>first name:</label>
 				<input type="text" name="fname" placeholder="first name">
 			</div>
+
 			<div>
+				<?php
+					if(isset($errors['lname'])) {echo '<span class="err">'. $errors['lname']. '</span>';}
+				?>
 				<label>last name:</label>	
 				<input type="text" name="lname" placeholder="last name">
 			</div>
 
 			<div>
+				<?php
+					if(isset($errors['email'])) {echo '<span class="err">'. $errors['email']. '</span>';}
+
+				?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
+
 			<div>
+				<?php
+					if(isset($errors['password'])) {echo '<span class="err">'. $errors['password']. '</span>';}
+				?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
  
 			<div>
+				<?php
+					if(isset($errors['pword'])) {echo '<span class="err">'. $errors['pword']. '</span>';}
+				?>
 				<label>confirm password:</label>	
 				<input type="password" name="pword" placeholder="password">
 			</div>
