6007739dfdb8dc4671d03575700d5c4157913bfd
dammy <adiodamilola@yahoo.com>
2017-04-03 13:39:06 +0100

    creat view product page

---
+++ www/viewprod.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>View Category</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+
+<body>
+	<section>
+		<div class="mast">
+			<h1>T<span>SSB</span></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="category.php" class="selected">Add Category</a></li>
+					<li><a href="products.php">Add Products</a></li>
+					<li><a href="viewcat.php">View Products</a></li>
+					<li><a href="#">logout</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+	<div class="wrapper">
+		<div id="stream">
+		<h1 id="register-label">Add Category</h1>
+			<table id="tab">
+				<thead>
+					<tr>
+						<th>title</th>
+						<th>author</th>
+						<th>category</th>
+						<th>price</th>
+						<th>publication date</th>
+						<th>isbn</th>
+						<th>image</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>the knowledge gap</td>
+						<td>maja</td>
+						<td>January, 10</td>
+						<td></td>
+						<td></td>
+						<td><a href="#">edit</a></td>
+						<td><a href="#">delete</a></td>
+					</tr>
+          		</tbody>
+			</table>
+		</div>
+
+		<div class="paginated">
+			<a href="#">1</a>
+			<a href="#">2</a>
+			<span>3</span>
+			<a href="#">2</a>
+		</div>
+	</div>
+
+	<section class="foot">
+		<div>
+			<p>&copy; 2016;
+		</div>
+	</section>
+</body>
+</html>
