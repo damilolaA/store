9f0295dcc15dcc4c2916498aa05dbc6af0930eb8
dammy <adiodamilola@yahoo.com>
2017-04-03 05:56:26 +0100

    product page header

---
+++ www/includes/header1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $page_title; ?></title>
+	<link rel="stylesheet" type="text/css" href="../styles/styles.css">
+</head>
+<body>
+	<section>
+		<div class="mast">
+			<h1>T<span>SSB</span></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="category.php" class="selected">Category</a></li>
+					<li><a href="#">Products</a></li>
+					<li><a href="#">Logout</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
