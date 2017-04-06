301b923ef34ec2b49a57f2739067c7bcc509cf37
dammy <adiodamilola@yahoo.com>
2017-04-06 14:30:15 +0100

    edit edit page

--- www/editprod.php
+++ www/editprod.php
@@ -13,7 +13,7 @@
 	$item = getBookByID($conn, $_GET['book_id']);
 	}
 
-	$category = getcategorybyID($conn, $item ['category_id']);
+	$category = getcategorybyID($conn, $item['category_name']);
 
 
 	if(array_key_exists('edit',$_POST)){
@@ -70,7 +70,7 @@
 				<label>category:</label>
 				<select name="category">
 				<option>Select Category</option>
-				<option value="<?php echo $category['category_id'];?>"><?php echo $category['category_name']; ?></option>
+				<option value="<?php echo $category['category_id'];?>"><?php echo $category['category_name'];?></option>
 				<?php
 					$catlist = doeditCategory($conn, $category['category_name']);
 					echo $catlist;
