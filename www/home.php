2c176ea5adbb390f774e9ff0a1a850d226b6c73c
dammy <adiodamilola@yahoo.com>
2017-04-08 06:34:34 +0100

    add header and footer to front home page

---
+++ www/home.php
@@ -0,0 +1,91 @@
+<?php
+
+    include 'includes/header2.php';
+
+?>
+   <div class="top-bar">
+    <div class="top-nav">
+      <a href="index.html"><h3 class="brand"><span>B</span>rain<span>F</span>ood</h3></a>
+      <ul class="top-nav-list">
+        <li class="top-nav-listItem Home"><a href="index.html">Home</a></li>
+        <li class="top-nav-listItem catalogue"><a href="catalogue.html">Catalogue</a></li>
+        <li class="top-nav-listItem login"><a href="login.html">Login</a></li>
+        <li class="top-nav-listItem register"><a href="registration.html">Register</a></li>
+        <li class="top-nav-listItem cart">
+          <div class="cart-item-indicator">
+            <p></p>
+          </div>
+          <a href="cart.html">Cart</a>
+        </li>
+      </ul>
+      <form class="search-brainfood">
+        <input type="text" class="text-field" placeholder="Search all books">
+      </form>
+    </div>
+  </div>
+  <!-- main content starts here -->
+  <div class="main">
+    <div class="book-display">
+      <div class="display-book"></div>
+      <div class="info">
+        <h2 class="book-title">Eloquent Javascript</h2>
+        <h3 class="book-author">by Marijn Haverbeke</h3>
+        <h3 class="book-price">$200</h3>
+
+        <form>
+          <label for="book-amout">Amount</label>
+          <input type="number" class="book-amount text-field">
+          <input class="def-button add-to-cart" type="submit" name="" value="Add to cart">
+        </form>
+      </div>
+    </div>
+    <div class="trending-books horizontal-book-list">
+      <h3 class="header">Trending</h3>
+      <ul class="book-list">
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$125</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$90</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$250</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$50</p></div>
+        </li>
+      </ul>
+    </div>
+    <div class="recently-viewed-books horizontal-book-list">
+      <h3 class="header">Recently Viewed</h3>
+      <ul class="book-list">
+        <div class="scroll-back"></div>
+        <div class="scroll-front"></div>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$250</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$50</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$125</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$90</p></div>
+        </li>
+      </ul>
+    </div>
+    
+  </div>
+  <!-- footer starts here-->
+ <?php
+  include 'includes/footer2.php';
+ ?>
