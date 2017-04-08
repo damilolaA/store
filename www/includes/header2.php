2743e7523b664c35873c03f4dd16c7efe53a0c70
dammy <adiodamilola@yahoo.com>
2017-04-08 06:35:45 +0100

    create header page

---
+++ www/includes/header2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="style/styles.css">
+    <title>Home</title>
+</head>
+<body id="home">
+  <!-- DO NOT TAMPER WITH CLASS NAMES! -->
+
+  <!-- top bar starts here -->
+ 
