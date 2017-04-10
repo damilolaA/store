4d3f0d4d8593d76a957a324d2b945df5004fe1f8
dammy <adiodamilola@yahoo.com>
2017-04-10 15:17:36 +0100

    adjust top bar

--- www/home.php
+++ www/home.php
@@ -10,26 +10,7 @@
          
 
 ?>
-   <div class="top-bar">
-    <div class="top-nav">
-      <a href="index.html"><h3 class="brand"><span>B</span>rain<span>F</span>ood</h3></a>
-      <ul class="top-nav-list">
-        <li class="top-nav-listItem Home"><a href="index.html">Home</a></li>
-        <li class="top-nav-listItem catalogue"><a href="catalogue.html">Catalogue</a></li>
-        <li class="top-nav-listItem login"><a href="login.html">Login</a></li>
-        <li class="top-nav-listItem register"><a href="registration.html">Register</a></li>
-        <li class="top-nav-listItem cart">
-          <div class="cart-item-indicator">
-            <p></p>
-          </div>
-          <a href="cart.html">Cart</a>
-        </li>
-      </ul>
-      <form class="search-brainfood">
-        <input type="text" class="text-field" placeholder="Search all books">
-      </form>
-    </div>
-  </div>
+  
   <!-- main content starts here -->
 
   <div class="main">
@@ -54,22 +35,7 @@
       <ul class="book-list">
         <div class="scroll-back"></div>
         <div class="scroll-front"></div>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$250</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$50</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$125</p></div>
-        </li>
-        <li class="book">
-          <a href="#"><div class="book-cover"></div></a>
-          <div class="book-price"><p>$90</p></div>
-        </li>
+        <?php $receive = recentlyviewed($conn); echo $receive; ?>
       </ul>
     </div>
     
