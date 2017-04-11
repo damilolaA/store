7c0fe2976b1b7734fbfb8ebb08e8698cab8c4712
dammy <adiodamilola@yahoo.com>
2017-04-11 01:13:59 +0100

    rework validation

--- www/userlogin.php
+++ www/userlogin.php
@@ -6,33 +6,34 @@
 
   include 'includes/header2.php';
 
+
+  $errors = [];
+
   if(array_key_exists('enter', $_POST)){
 
-      $errors = [];
-
       if(empty($_POST['email'])){
-        $errors[] = "Please enter email address";
+        $errors['email'] = "Please enter email address";
       }
 
       if(empty($_POST['pass'])){
-        $errors[] = "Please enter password";
+        $errors['pass'] = "Please enter your password";
       }
 
       if(empty($errors)){
 
         $clean = array_map('trim', $_POST);
 
-       $fit = userlogin($conn, $clean);
+        $get = userlogin($conn, $clean);
 
-       if($fit[0]){
+        if($get[0]){
 
-              $_SESSION['id'] = $fit[1]['user_id'];
-              $_SESSION['email'] = $fit[1]['email'];
-              redirect("home.php");
-       }else {
+            $_SESSION['id'] = $get[1]['user_id'];
+            $_SESSION['email'] = $get[1]['email'];
+            redirect("home.php");
+        }else {
 
-            redirect("userlogin.php?msg = invalid email and/or password");
-       }
+            redirect("userlogin.php? msg = invalid email and/or password");
+        }
       }
   }
 
@@ -41,14 +42,14 @@
   <!-- main content starts here -->
   <div class="main">
     <div class="login-form">
-      <form class="def-modal-form" action="userlogin.php" method="POST">
+      <form class="def-modal-form" action="userlogin.php" method="post">
         <div class="cancel-icon close-form"></div>
         <label for="login-form" class="header"><h3>Login</h3></label>
         <input type="text" class="text-field email" name="email" placeholder="Email">
-  <!--      <p class="form-error">invalid email</p>   -->
+  <     <p class="form-error"><?php $reveal = showerrors($errors, 'email'); echo $reveal; ?></p>  
         <input type="password" class="text-field password" name="pass" placeholder="Password">
-        <!--clear the error and use it later just to show you how it works 
-        <p class="form-error">wrong password</p>  -->
+        <!--clear the error and use it later just to show you how it works   -->
+        <p class="form-error"><?php $bring = showerrors($errors,'pass'); echo $bring; ?></p> 
         <input type="submit" class="def-button login" name="enter" value="Login">
       </form>
     </div>
